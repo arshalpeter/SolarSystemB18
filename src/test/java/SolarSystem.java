8e1a9b1acb0b0d4c6cda7ac69071f9319f21307f
arshalpeter <149265122+arshalpeter@users.noreply.github.com>
2024-02-17 12:41:49 -0500

    planet2

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -4,9 +4,17 @@ public class SolarSystem {
         solar.Stars="100";
         solar.Moon="4";
         solar.Planets="2";
+
         FeatureSun xros= new FeatureSun();
         xros.Color="pink";
         xros.temp="23243243243234k";
         xros.Size="234243432432432424miles";
+
+        planet2 gecko =new planet2();
+        gecko.color="magenta";
+        gecko.temp="34343443434k";
+        gecko.size="342344342m";git 
+
+
     }
 }
